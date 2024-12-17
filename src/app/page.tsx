af9845359ced93aa137f472051ef3f92e9a33d86
unknown <felipescherer25@gmail.com>
2024-12-16 21:58:49 -0300

    :sparkles: feat first comp

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
+import { DeckForm } from '@/components/deck-form'
+
 export default function Home() {
   return (
     <main className="flex justify-center p-4">
-      <div>
-        <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
-      </div>
+      <DeckForm />
 
       <div>
         <div />
---
+++ src/components/deck-form.tsx
@@ -0,0 +1,8 @@
+export function DeckForm() {
+  return (
+    <div>
+
+      <input type="text" placeholder="Type here" className="input input-bordered w-full max-w-xs" />
+    </div>
+  )
+}
